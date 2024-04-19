a4ebaf9a5f6d2734219fe974737edd699b69c366
DhirenKirpalani <dhirenkirpalani2308@gmail.com>
2024-04-19 13:32:33 +0700

    chore: update navigation

--- src/navigators/Navigation.js
+++ src/navigators/Navigation.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
+import AuthOptionsScreen from "../screens/AuthOptionsScreen";
 
 const Stack = createNativeStackNavigator()
 
@@ -9,6 +10,7 @@ export default function navigation() {
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name="Welcome" component={WelcomeScreen} options={{ headerShown: false }}/>
+                <Stack.Screen name="AuthOptions" component={AuthOptionsScreen} options={{ headerShown: false }}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
