b34ebc46063797bb2596f6e0b5a3bb9d7ffaa65e
DhirenKirpalani <dhirenkirpalani2308@gmail.com>
2024-04-20 17:40:45 +0700

    chore: create WeightSelection Screen

--- src/navigators/Navigation.js
+++ src/navigators/Navigation.js
@@ -7,6 +7,7 @@ import AgeSelectionScreen from "../screens/AgeSelectionScreen";
 import HeightSelectionScreen from "../screens/HeightSelectionScreen";
 import GoalSelectionScreen from "../screens/GoalSelectionScreen";
 import LevelSelectionScreen from "../screens/LevelSelectionScreen";
+import WeightSelectionScreen from "../screens/WeightSelectionScreen";
 
 const Stack = createNativeStackNavigator()
 
@@ -19,8 +20,9 @@ export default function navigation() {
                 <Stack.Screen name="GenderSelectionScreen" component={GenderSelectionScreen} options={{ headerShown: false }}/>
                 <Stack.Screen name="AgeSelectionScreen" component={AgeSelectionScreen} options={{ headerShown: false }}/>
                 <Stack.Screen name="HeightSelection" component={HeightSelectionScreen} options={{ headerShown: false }}/>
-                <Stack.Screen name="GoalSelectionScreen" component={GoalSelectionScreen} options={{ headerShown: false }}/> */}
-                <Stack.Screen name="LevelSelectionScreen" component={LevelSelectionScreen} options={{ headerShown: false }}/>
+                <Stack.Screen name="GoalSelectionScreen" component={GoalSelectionScreen} options={{ headerShown: false }}/>
+                <Stack.Screen name="LevelSelectionScreen" component={LevelSelectionScreen} options={{ headerShown: false }}/> */}
+                <Stack.Screen name="WeightSelectionScreen" component={WeightSelectionScreen} options={{ headerShown: false }}/>
             </Stack.Navigator>
         </NavigationContainer>
     )
