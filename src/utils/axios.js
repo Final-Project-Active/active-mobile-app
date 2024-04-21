e9560330a495b6e2df4fa4b1323b83bd1e830aa8
artharestu <artharestuabadi@gmail.com>
2024-04-21 15:12:40 +0700

    Install & Config Axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const serverRequest = axios.create({
   // baseURL: 'http://localhost:3000'
-  baseURL: 'http://13.229.104.123/'
+  baseURL: 'http://13.229.104.123'
 })
