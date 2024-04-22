878b43b79605340582e000cb59c5e9931378d78b
artharestu <artharestuabadi@gmail.com>
2024-04-22 14:05:41 +0700

    FIX: Image URL Registration

--- src/screens/LevelSelectionScreen.js
+++ src/screens/LevelSelectionScreen.js
@@ -24,7 +24,11 @@ export default function LevelSelectionScreen({ navigation, route }) {
       await serverRequest({
         method: "post",
         url: "/register",
-        data: { ...route.params, physicalActivity: selectedLevel },
+        data: {
+          ...route.params,
+          physicalActivity: selectedLevel,
+          imageUrl: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+        },
       });
       navigation.navigate('SignInScreen');
     } catch (error) {
